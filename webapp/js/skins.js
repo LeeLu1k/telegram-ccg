4ea35a458bc700b674c34e80379d1d1248a654a9
LeeLu1k <119786889+LeeLu1k@users.noreply.github.com>
2025-11-04 19:31:22 +0500

    Update skins.js

--- webapp/js/skins.js
+++ webapp/js/skins.js
@@ -1,6 +1,16 @@
 export const skins = [
-  { id: 1, name: "Спайкли", type: "Стрелок", hp: 1200, attack: 180, defense: 40, price: 0, image: "/webapp/img/skins/spikly.png" },
-  { id: 2, name: "Буллит", type: "Танк", hp: 2000, attack: 160, defense: 80, price: 150, image: "/webapp/img/skins/bullit.png" },
-  { id: 3, name: "Лунара", type: "Маг", hp: 1300, attack: 210, defense: 50, price: 200, image: "/webapp/img/skins/lunara.png" },
-  // и т.д.
+  {
+    id: 0,
+    name: "Спайкли",
+    image: "img/skins/spikly.png",
+    hp: 1000,
+    attack: 120,
+  },
+  {
+    id: 1,
+    name: "Буллит",
+    image: "img/skins/bullit.png",
+    hp: 900,
+    attack: 140,
+  }
 ];
