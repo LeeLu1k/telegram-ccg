851daadc6dc0959d3aede5a56f2d1a2797c157c3
LeeLu1k <119786889+LeeLu1k@users.noreply.github.com>
2025-11-04 18:23:06 +0500

    Update main.js

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -1,2 +1,10 @@
+// /webapp/js/main.js
 import { initUser } from './utils.js';
+import { cards } from './skins.js';
+
 initUser();
+
+const hero = cards[0]; // первый герой — подарок новичку
+document.querySelector("#user-photo").src = hero.image;
+document.querySelector("h1").textContent = hero.name;
+document.querySelector("p").textContent = `${hero.type} • HP: ${hero.hp} • Атака: ${hero.attack}`;
