37a30430f53e93f7f129d4273d0d238be170ab88
LeeLu1k <119786889+LeeLu1k@users.noreply.github.com>
2025-11-04 16:22:53 +0500

    Update app.js

--- webapp/app.js
+++ webapp/app.js
@@ -1,1 +1,15 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width,initial-scale=1"/>
+  <title>CCG WebApp</title>
+</head>
+<body>
+  <h1>Моя карточная игра (Web App)</h1>
+  <div id="game"></div>
+  <button id="sendState">Сохранить состояние</button>
 
+  <script src="app.js"></script>
+</body>
+</html>
