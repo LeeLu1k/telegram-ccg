64de4a7bb9de0b08680710d073bb7bda61784e3b
LeeLu1k <119786889+LeeLu1k@users.noreply.github.com>
2025-11-05 01:06:36 +0500

    Update user.js

--- webapp/js/user.js
+++ webapp/js/user.js
@@ -2,15 +2,11 @@ export function getUser() {
   const data = localStorage.getItem('user');
   if (data) return JSON.parse(data);
   // при первом входе
-  const newUser = { balance: 0, level: 1, selectedSkin: 0 };
-  localStorage.setItem('user', JSON.stringify(newUser));
+  const data = JSON.parse(localStorage.getItem("userData"));
+  return data || { balance: 0, level: 1, xp: 0, selectedSkin: 0 };
   return newUser;
 }
 
 export function saveUser(user) {
   localStorage.setItem('user', JSON.stringify(user));
-}
-export function getUser() {
-  const data = JSON.parse(localStorage.getItem("userData"));
-  return data || { balance: 0, level: 1, xp: 0, selectedSkin: 0 };
 }
